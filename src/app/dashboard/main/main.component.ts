6bf66d2a6271c85e9cf4ecae980a6163c87853a4
Cecilia Pacheco <al063840@uacam.mx>
2024-11-30 14:03:51 -0600

    Página de Rollitos
    Creación de nuevos documentos, sidebar y otros componentes

--- src/app/dashboard/main/main.component.ts
+++ src/app/dashboard/main/main.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,HostListener, OnInit  } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
+
 export class MainComponent {
+  isSmallScreen: boolean = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isSmallScreen = window.innerWidth <= 768;
+  }
+
+  onSidenavOpen() {
+    console.log('Sidebar abierto');
+  }
+
+  onSidenavClose() {
+    console.log('Sidebar cerrado');
+  }
+
+
+  onButtonClick(): void {
+    console.log("Botón flotante presionado");
+    // Aquí puedes agregar la lógica que desees, como redirigir a otra página o mostrar un mensaje.
+  }
 
 }
